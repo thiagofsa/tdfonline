0c681e909a7c22c3baf06d6f8fd91eb3bb63c371
thiagofsa <39659783+thiagofsa@users.noreply.github.com>
2018-05-28 21:43:47 -0300

    Update HibernateConf.java

--- tdfonline2/src/br/com/tfdonline/config/HibernateConf.java
+++ tdfonline2/src/br/com/tfdonline/config/HibernateConf.java
@@ -1,6 +1,6 @@
 package br.com.tfdonline.config;
 //teste3dsfsf
-//alteirei essa porra...
+//alteradodo github
 import java.io.IOException;
 import java.util.Properties;
 import javax.sql.DataSource;
